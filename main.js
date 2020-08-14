65cded1c2a36948a033913eb548925598ce99bf4
Manuchehr Shamsiddinov <manu_shams99@mail.ru>
2020-08-14 17:37:36 +0500

    opaque overlay

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menu')
-const menuStripe = document.querySelectorAll('.menu-stripe')
+const menuStripe = document.querySelectorAll('.menu-stripe');
+const overlay = document.querySelector('.overlay')
 
 var open = false
 
@@ -7,13 +8,11 @@ var open = false
 menu.addEventListener('click', function addClass(){
         if(!open){
             menu.classList.add('click')
+            overlay.classList.remove('invisible')
             open = true
         }else{
             menu.classList.remove('click')
+            overlay.classList.add('invisible')
             open = false
         }
-        
-
-        
-        
     })
